844cb3a80e96f7a82febfef45859b6aa4ffb5d3d
Isacc Hernandez <isacc1798@gmail.com>
2024-11-22 23:17:21 -0700

    Added login page GET

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -10,12 +10,13 @@ import (
 // Define a new Validator struct which contains a map of validation error messages
 // for our form fields.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
 // AddFieldError() adds an error message to the FieldErrors map (so long as no
@@ -70,4 +71,8 @@ func MinChars(value string, n int) bool {
 // expression pattern
 func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
